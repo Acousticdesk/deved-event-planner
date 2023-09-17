836dc5cc37fa819bd7229289558df426706e2314
Andrii Beha <95135544+andriibeha@users.noreply.github.com>
2023-09-17 15:09:36 -0600

    add Edmonton Event Point to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,78 @@
-import TextField from '@mui/material/TextField'
-import Container from '@mui/material/Container'
-import { collection, getDocs, GeoPoint } from 'firebase/firestore/lite';
-import { useEffect, useState } from 'react'
-import dayjs from 'dayjs'
-import { Calendar, dayjsLocalizer } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { db } from './api.ts';
-import { Layout } from './components/Layout.tsx';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import dayjs from "dayjs";
+import { GeoPoint, collection, getDocs } from "firebase/firestore/lite";
+import { useEffect, useState } from "react";
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { db } from "./api.ts";
+import { Layout } from "./components/Layout.tsx";
 
 interface Event {
-  event_name: string
-  event_date: number
-  event_location: GeoPoint
+  event_name: string;
+  event_date: number;
+  event_location: GeoPoint;
 }
 
 interface Inputs {
-  search: string
+  search: string;
 }
 
-const djLocalizer = dayjsLocalizer(dayjs)
+const djLocalizer = dayjsLocalizer(dayjs);
 
 function App() {
   const [events, setEvents] = useState<Event[]>([]);
-  const { register, watch } = useForm<Inputs>()
-  const navigate = useNavigate()
+  const { register, watch } = useForm<Inputs>();
+  const navigate = useNavigate();
   useEffect(() => {
-    const eventsCollection = collection(db, 'events')
+    const eventsCollection = collection(db, "events");
     getDocs(eventsCollection).then((snapshot) => {
-      const list = snapshot.docs.map(doc => doc.data() as Event)
-      setEvents(list)
+      const list = snapshot.docs.map((doc) => doc.data() as Event);
+      setEvents(list);
     });
-  }, [])
-  
-  const search = watch('search')
-  
+  }, []);
+
+  const search = watch("search");
+
   return (
     <Layout>
       <Container>
         <div className="p-8 flex items-center justify-center">
-          <TextField placeholder="Search..." inputProps={{ className: "px-2 py-1", ...register('search') }} />
+          <TextField
+            placeholder="Search..."
+            inputProps={{ className: "px-2 py-1", ...register("search") }}
+          />
         </div>
-        <p className="text-4xl text-center mb-2">All Events</p>
+        <p className="text-2xl text-center mb-2">All Events</p>
         <Calendar
           localizer={djLocalizer}
           scrollToTime={dayjs().toDate()}
           onSelectEvent={(event) => {
-            navigate(`/events/${event.title}`)
+            navigate(`/events/${event.title}`);
           }}
-          events={events.filter(({ event_name }) => event_name.toLowerCase().includes(search)).map(({ event_name, event_date }) => {
-            const endDate = new Date(event_date)
-            endDate.setHours(endDate.getHours() + 1)
-            return {
-              id: event_name,
-              title: event_name,
-              start: new Date(event_date),
-              end: new Date(endDate),
-            }
-          })}
+          events={events
+            .filter(({ event_name }) =>
+              event_name.toLowerCase().includes(search),
+            )
+            .map(({ event_name, event_date }) => {
+              const endDate = new Date(event_date);
+              endDate.setHours(endDate.getHours() + 1);
+              return {
+                id: event_name,
+                title: event_name,
+                start: new Date(event_date),
+                end: new Date(endDate),
+              };
+            })}
           startAccessor="start"
           endAccessor="end"
           style={{ height: 500 }}
         />
       </Container>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/CreateEvent.tsx
+++ src/pages/CreateEvent.tsx
@@ -1,141 +1,152 @@
-import { Layout } from '../components/Layout.tsx';
-import Container from '@mui/material/Container';
-import Checkbox from '@mui/material/Checkbox'
-import Button from '@mui/material/Button'
-import CircularProgress from '@mui/material/CircularProgress'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import { useForm, SubmitHandler, Controller } from "react-hook-form"
-import dayjs from 'dayjs';
-import { useState, FormEvent, useCallback, useEffect } from "react"
-import { useNavigate, useParams } from 'react-router-dom';
-import { db } from '../api.ts';
-import { addDoc, getDocs, collection, query, where } from 'firebase/firestore/lite';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import CircularProgress from "@mui/material/CircularProgress";
+import Container from "@mui/material/Container";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import dayjs from "dayjs";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore/lite";
+import { FormEvent, useCallback, useEffect, useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import { db } from "../api.ts";
+import { Layout } from "../components/Layout.tsx";
 
-import TextField from '@mui/material/TextField'
-import { Toast } from '../components/Toast.tsx';
+import TextField from "@mui/material/TextField";
+import { Toast } from "../components/Toast.tsx";
 
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { Dayjs } from "dayjs";
 import { Configuration, OpenAIApi } from "openai";
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { Dayjs } from 'dayjs';
 
 interface Inputs {
-  event_name: string
-  event_date: number
-  event_location: string
-  event_image: string
-  event_description: string
+  event_name: string;
+  event_date: number;
+  event_location: string;
+  event_image: string;
+  event_description: string;
 }
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: "400px",
+  height: "400px",
 };
 
 export function CreateEvent() {
-  const { id } = useParams()
-  
-  const {
-    register,
-    watch,
-    handleSubmit,
-    setValue,
-    control
-  } = useForm<Inputs>({
+  const { id } = useParams();
+
+  const { register, watch, handleSubmit, setValue, control } = useForm<Inputs>({
     defaultValues: async () => {
       if (id) {
-        const q = query(collection(db, 'events'), where('event_name', '==', id))
+        const q = query(
+          collection(db, "events"),
+          where("event_name", "==", id),
+        );
 
-        const snapshot = await getDocs(q)
-        const res = snapshot.docs[0].data()
-        setValue('event_image', res?.event_image)
-        return snapshot.docs[0].data() as Inputs
+        const snapshot = await getDocs(q);
+        const res = snapshot.docs[0].data();
+        setValue("event_image", res?.event_image);
+        return snapshot.docs[0].data() as Inputs;
       }
       return {
         event_date: Date.now(),
-        event_description: '',
-        event_image: '',
-        event_location: '',
-        event_name: ''
-      } as Inputs
-    }
-  })
-  
-  const eventName = watch('event_name')
-  const eventImage = watch('event_image')
-  const eventLocation = watch('event_location')
-  
-  const [hasToast, setHasToast] = useState(false)
-  const [isEventImageLoading, setIsEventImageLoading] = useState(false)
-  const [eventImageError, setEventImageError] = useState('')
+        event_description: "",
+        event_image: "",
+        event_location: "",
+        event_name: "",
+      } as Inputs;
+    },
+  });
+
+  const eventName = watch("event_name");
+  const eventImage = watch("event_image");
+  const eventLocation = watch("event_location");
+
+  const [hasToast, setHasToast] = useState(false);
+  const [isEventImageLoading, setIsEventImageLoading] = useState(false);
+  const [eventImageError, setEventImageError] = useState("");
 
   const center = {
-    lat: eventLocation ? parseFloat(eventLocation.split(',')[0]) : 53.54257718389261,
-    lng: eventLocation ? parseFloat(eventLocation.split(',')[1]) : -113.49767851787895,
+    lat: eventLocation
+      ? parseFloat(eventLocation.split(",")[0])
+      : 53.54257718389261,
+    lng: eventLocation
+      ? parseFloat(eventLocation.split(",")[1])
+      : -113.49767851787895,
   };
-  
-  const navigate = useNavigate()
+
+  const navigate = useNavigate();
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     await addDoc(collection(db, "events"), data);
-    setHasToast(true)
+    setHasToast(true);
     window.setTimeout(() => {
-      navigate('/')
-    }, 2000)
-  }
-  
+      navigate("/");
+    }, 2000);
+  };
+
   const generateImage = async () => {
-    setEventImageError('')
+    setEventImageError("");
     try {
-      const q = query(collection(db, 'keys'))
-      const snapshot = await getDocs(q)
-      const { key } = snapshot.docs[0].data()
+      const q = query(collection(db, "keys"));
+      const snapshot = await getDocs(q);
+      const { key } = snapshot.docs[0].data();
       const configuration = new Configuration({
         apiKey: key,
       });
       const openai = new OpenAIApi(configuration);
-      setIsEventImageLoading(true)
-      const response = await openai.createImage({
-        prompt: `professional photo from event ${eventName}`,
-        n: 1,
-        size: "256x256",
-      }).finally(() => {
-        setIsEventImageLoading(false)
-      });
+      setIsEventImageLoading(true);
+      const response = await openai
+        .createImage({
+          prompt: `professional photo from event ${eventName}`,
+          n: 1,
+          size: "256x256",
+        })
+        .finally(() => {
+          setIsEventImageLoading(false);
+        });
       return response.data.data[0].url;
     } catch (e) {
-      setEventImageError("Can't generate your image now, please try again later...")
+      setEventImageError(
+        "Can't generate your image now, please try again later...",
+      );
     }
-  }
-  
+  };
+
   const handleAIGenerateImageChange = async (e: FormEvent) => {
     if (!(e.target as HTMLInputElement).checked) {
-      setValue('event_image', '')
-      return
+      setValue("event_image", "");
+      return;
     }
-    const url = await generateImage() as string
-    setValue('event_image', url)
-  }
-  
-  const pageTitle = id ? eventName : "Event Creation Form"
+    const url = (await generateImage()) as string;
+    setValue("event_image", url);
+  };
+
+  const pageTitle = id ? eventName : "Event Creation Form";
 
   const { isLoaded: isMapLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyAmwORDeV0GTzkwHOJq0s6iecfaTPIOgJE"
-  })
-  
-  const [map, setMap] = useState<google.maps.Map | null>(null)
-  const [marker, setMarker] = useState<google.maps.Marker | null>(null)
-  
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyAmwORDeV0GTzkwHOJq0s6iecfaTPIOgJE",
+  });
+
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+
   useEffect(() => {
     if (id && map) {
       const location = new google.maps.LatLng(center.lat, center.lng);
 
       const bounds = new window.google.maps.LatLngBounds(center);
       map.fitBounds(bounds);
-      
+
       if (marker) {
-        marker.setMap(null)
+        marker.setMap(null);
       }
 
       const newMarker = new window.google.maps.Marker({
@@ -143,20 +154,20 @@ export function CreateEvent() {
         map: map,
       });
 
-      setMarker(newMarker)
+      setMarker(newMarker);
     }
-  }, [map, center.lng, center.lat])
+  }, [map, center.lng, center.lat]);
 
   const onLoad = useCallback(function callback(map: google.maps.Map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = useCallback(function callback() {
-    setMap(null)
-  }, [])
-  
+    setMap(null);
+  }, []);
+
   return (
     <Layout>
       <Container className="py-8">
@@ -165,7 +176,11 @@ export function CreateEvent() {
           <form onSubmit={handleSubmit(onSubmit)}>
             {id && eventImage && (
               <div className="mb-4">
-                <img src={eventImage} alt="Event Image" className="block m-auto" />
+                <img
+                  src={eventImage}
+                  alt="Event Image"
+                  className="block m-auto"
+                />
               </div>
             )}
             <TextField
@@ -176,11 +191,11 @@ export function CreateEvent() {
               InputLabelProps={{
                 shrink: true,
               }}
-              {...register('event_name')}
+              {...register("event_name")}
             />
             <Controller
               control={control}
-              name='event_date'
+              name="event_date"
               render={({ field }) => (
                 <DateTimePicker
                   label="Event Time"
@@ -189,10 +204,10 @@ export function CreateEvent() {
                   value={dayjs(field.value)}
                   onChange={(date: Dayjs | null) => {
                     if (!date || !date.isValid()) {
-                      setValue('event_date', Date.now())
-                      return
+                      setValue("event_date", Date.now());
+                      return;
                     }
-                    field.onChange(date.unix() * 1000)
+                    field.onChange(date.unix() * 1000);
                   }}
                 />
               )}
@@ -205,7 +220,7 @@ export function CreateEvent() {
               InputLabelProps={{
                 shrink: true,
               }}
-              {...register('event_description')}
+              {...register("event_description")}
             />
             <div className="text-center">
               <div className="inline-block py-4">
@@ -216,15 +231,15 @@ export function CreateEvent() {
                     onUnmount={onUnmount}
                     zoom={5}
                     options={{
-                      maxZoom: 17
+                      maxZoom: 17,
                     }}
                     onClick={(event) => {
                       if (id) {
-                        return
+                        return;
                       }
                       if (marker) {
-                        marker.setMap(null)
-                        setMarker(null)
+                        marker.setMap(null);
+                        setMarker(null);
                       }
 
                       const newMarker = new window.google.maps.Marker({
@@ -232,15 +247,26 @@ export function CreateEvent() {
                         map: map,
                       });
 
-                      setMarker(newMarker)
-                      setValue('event_location', `${event.latLng?.lat()},${event.latLng?.lng()}`)
+                      setMarker(newMarker);
+                      setValue(
+                        "event_location",
+                        `${event.latLng?.lat()},${event.latLng?.lng()}`,
+                      );
                     }}
                   />
-                ) : <CircularProgress />}
+                ) : (
+                  <CircularProgress />
+                )}
               </div>
             </div>
             <div className="text-center">
-              {!id && eventImage && <img src={eventImage} alt="Event Image" className="block m-auto" />}
+              {!id && eventImage && (
+                <img
+                  src={eventImage}
+                  alt="Event Image"
+                  className="block m-auto"
+                />
+              )}
               {isEventImageLoading && <CircularProgress />}
               {eventImageError && <p>{eventImageError}</p>}
             </div>
@@ -250,13 +276,20 @@ export function CreateEvent() {
                   control={<Checkbox onChange={handleAIGenerateImageChange} />}
                   label="AI-generated thumbnail"
                 />
-                <Button disabled={hasToast} type="submit">Submit</Button>
+                <Button disabled={hasToast} type="submit">
+                  Submit
+                </Button>
               </div>
             )}
-            {hasToast && <Toast severity="success" message={`${eventName} event was created`} />}
+            {hasToast && (
+              <Toast
+                severity="success"
+                message={`${eventName} event was created`}
+              />
+            )}
           </form>
         </div>
       </Container>
     </Layout>
-  )
+  );
 }
