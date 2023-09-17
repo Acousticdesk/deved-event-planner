32b22fd5c34257d2b3858bcfcf0521cec07245c3
Andrii Kicha <akicha@star.global>
2023-09-16 18:12:15 -0600

    add calendar view to the home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import TextField from '@mui/material/TextField'
 import Container from '@mui/material/Container'
 import { getFirestore, collection, getDocs, Timestamp, GeoPoint } from 'firebase/firestore/lite';
 import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
 import { Header } from './components/Header.tsx';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { app } from './api.ts';
 
 interface Event {
@@ -10,6 +13,8 @@ interface Event {
   event_date: Timestamp
   event_location: GeoPoint
 }
+
+const djLocalizer = dayjsLocalizer(dayjs)
 
 function App() {
   const [events, setEvents] = useState<Event[]>([]);
@@ -29,16 +34,19 @@ function App() {
         <div className="p-8 flex items-center justify-center">
           <TextField placeholder="Search..." inputProps={{ className: "px-2 py-1" }} />
         </div>
-        <p className="text-4xl text-center">All Events</p>
-        <div>
-          {events.map(({ event_name }) => {
-            return (
-              <div key={event_name}>
-                <p>{event_name}</p>
-              </div>
-            )
-          })}
-        </div>
+        <p className="text-4xl text-center mb-2">All Events</p>
+        <Calendar
+          localizer={djLocalizer}
+          events={events.map(({ event_name }) => ({
+            id: event_name,
+            title: event_name,
+            start: new Date(2023, 8, 15),
+            end: new Date(2023, 8, 15),
+          }))}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
       </Container>
     </div>
   )
