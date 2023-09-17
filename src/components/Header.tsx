ed066a86298e73cfffff6fe2de2a0edaac2236d2
Andrii Beha <95135544+andriibeha@users.noreply.github.com>
2023-09-17 11:35:26 -0600

    add logo.svg and creat Logo.tsx component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
-import { Link, useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button'
+import Button from "@mui/material/Button";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../assets/Logo";
 
 export function Header() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="border-b border-black">
       <div className="flex justify-between">
-        <Link to="/" className="text-3xl">Logo</Link>
-        <Button onClick={() => navigate('/create-event')}>Create Event</Button>
+        <Link to="/" className="text-3xl">
+          {" "}
+          <Logo></Logo>{" "}
+        </Link>
+        <Button onClick={() => navigate("/create-event")}>Create Event</Button>
       </div>
     </div>
-  )
+  );
 }
